9dcf0b8cf7e06738c3675a7faea08ccc584ff3b4
William Muli <wmuli@tikenya.org>
2017-08-18 15:19:31 +0300

    Added a simple migration script

--- migrate.sh
+++ migrate.sh
@@ -2,7 +2,7 @@
 #create a copy of datasources.json called datasources1.json
 cp server/datasources.json server/datasources1.json
 #copy datasources.production.json to datasources.json
-cp cp server/datasources.production.json server/datasources.json
+cp server/datasources.production.json server/datasources.json
 #migrate the database
 lb-migration migrate --ds=mysql --method=update
 #recreate original datasources.json
