4eb79bcbe26428b8419958466cd29ed8d37d70d7
munyasi <psmunyasi@gmail.com>
2017-10-18 11:07:38 +0300

    shareholder list

--- server/shareholders/generate_list.js
+++ server/shareholders/generate_list.js
@@ -4,6 +4,7 @@ let fs = require('fs');
 let path = require('path');
 let JSZip = require('jszip');
 let Docxtemplater = require('docxtemplater');
+let Excel = require('exceljs');
 let _ = require('underscore');
 let uuid = require('uuid/v4');
 
@@ -11,8 +12,17 @@ function generateList (company_id, export_config, res, cb) {
 	let app = require('../server');
 	let Shareholder = app.models.Shareholder;
 	let Company = app.models.Company;
-	let template_path = '../templates/list_of_shareholders.docx';
-	let output_path = '../output/shareholders_list';
+	let template_path = '';
+    let output_path = '../output/shareholders_list';
+	if(export_config.type=='doc'){
+        template_path = '../templates/list_of_shareholders.docx';
+
+    }else if(export_config.type=='xlsx'){
+        template_path = '../templates/list_of_shareholders.xlsx';
+	}else{
+    	return cb('No such file type');
+	}
+
 	let sortByShares = false;
 
 	let findCompanyPromise = Company.findById(company_id,
@@ -37,9 +47,14 @@ function generateList (company_id, export_config, res, cb) {
 				shareholders = _.sortBy(shareholders, 'total_shares').reverse();
 			}
 
-			let createDocumentPromise = createDocument(shareholders, company);
-			createDocumentPromise.then((data) => { cb(null, data);});
-			createDocumentPromise.catch((err) => { cb(err);});
+            if(export_config.type=='doc'){
+                let createDocumentPromise = createWordDocument(shareholders, company);
+                createDocumentPromise.then((data) => { cb(null, data);});
+                createDocumentPromise.catch((err) => { cb(err);});
+
+            }else if(export_config.type=='xlsx'){
+                let createDocumentPromise = createExcelDocument(shareholders, company);
+            }
 		});
 
 		findShareholderPromise.catch((err) => { cb(err);});
@@ -82,7 +97,7 @@ function generateList (company_id, export_config, res, cb) {
 	 * @param company
 	 * @Return A promise
 	 */
-	function createDocument (shareholders, company) {
+	function createWordDocument (shareholders, company) {
 		let content = fs.readFileSync(path.resolve(__dirname, template_path), 'binary');
 		let zip = new JSZip(content);
 		let doc = new Docxtemplater();
@@ -104,7 +119,7 @@ function generateList (company_id, export_config, res, cb) {
 				let buf = doc.getZip().generate({type: 'nodebuffer'});
 				let token = uuid().toString().substring(0, 7);
 				let fileName = `List-of-shareholders-${company.company_name}-${token}.docx`;
-				console.log(fileName);
+				//console.log(fileName);
 				fs.writeFileSync(path.resolve(__dirname, `${output_path}/${fileName}`), buf);
 				return resolve({success: 1, path: fileName});
 			}
@@ -121,6 +136,41 @@ function generateList (company_id, export_config, res, cb) {
 
 		return new Promise(createDocPromise);
 	}
+
+
+	function createExcelDocument(shareholders, company) {
+        let workbook = new Excel.Workbook();
+
+        workbook.xlsx.readFile(path.resolve(__dirname, template_path))
+            .then(function() {
+                let token = uuid().toString().substring(0, 7);
+                let filename = `List-of-shareholders-${company.company_name}-${token}.xlsx`;
+                let worksheet = workbook.getWorksheet(1);
+
+                worksheet.getRow(1).getCell(1).value = company.company_name;
+                worksheet.getRow(2).getCell(1).value = `P.O BOX ${company.ro_postal_code}- ${company.ro_postal_address}`;
+                worksheet.getRow(3).getCell(1).value = company.ro_town_city;
+                let rowCount = 5;
+
+				_.forEach(shareholders,(value,index) => {
+					let row = worksheet.getRow(rowCount);
+					value = JSON.parse(JSON.stringify(value));
+					row.getCell(1).value = value.id;
+					row.getCell(2).value = value.name;
+					row.getCell(3).value = value.email_address;
+					row.getCell(4).value = `P.O BOX ${value.postal_code}- ${value.box}, ${value.town}`;
+					row.getCell(5).value = value.total;
+					row.commit();
+					rowCount++;
+				})
+				workbook.xlsx.writeFile(path.resolve(__dirname, `${output_path}/${filename}`));
+				cb(null,{success: 1, path: filename});
+
+            })
+            .catch(function (err) {
+                cb(err);
+            })
+    }
 }
 
 module.exports = generateList;
